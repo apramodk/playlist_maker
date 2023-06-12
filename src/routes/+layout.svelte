7d4f7080bb8e145a591f4925e78a1208d701c7dd
apramodk <apramodk@vols.utk.edu>
2023-06-12 18:15:32 -0400

    template site with login setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css"
+    import { page } from '$app/stores';
+</script>
+
+<slot/>
+
+<style>
+    :global(html) {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
